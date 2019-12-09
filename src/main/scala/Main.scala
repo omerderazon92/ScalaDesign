1141f94c41c0ad150950df7d0650e803768913ee
Omer Derazon <omerderazon92@gmail.com>
2019-12-09 15:29:15 +0200

    Standalone parser

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import project.configuration.{ConfigurationFetcher, DevName, ConfigurationName}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    ConfigurationFetcher.apply(ConfigurationName.Cliff, DevName.OmerD)
+    ConfigurationFetcher.apply(ConfigurationName.Cliff)
     ConfigurationFetcher.fetchConfiguration()
   }
 }
